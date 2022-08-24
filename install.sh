bca4dbd8932865ed3d6f7c78eefca43d9fa8c750
kkbaozi520 <95422814+kkbaozi520@users.noreply.github.com>
2022-08-24 23:24:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-if [ ! $Graffiti ]; then
-	read -p "Enter your Graffiti: " Graffiti
-	echo 'export Graffiti='${Graffiti} >> $HOME/.bash_profile
-fi
-echo -e '\n\e[45mYour Graffiti:' $Graffiti '\e[0m\n'
-sudo rm -rf /data/ironfish/.ironfish
-sudo docker run -it -v /data/ironfish/.ironfish:/root/.ironfish --name ironfish_node --restart always  -p "8120:8020" -p "9033:9033" -d qsobad/ironfish_node:nighty
-sudo docker exec -it  ironfish_node ironfish config:set blockGraffiti "$Graffiti"
-nohup sh $HOME/ironfish/restart.sh >> $HOME/ironfish/ironfish.log &
+ps -ef | grep "restart.sh" | awk '{print $2}' | xargs kill -9
+nohup sh $HOME/ironfish_1/restart.sh >> $HOME/ironfish_1/ironfish.log &
 ls
