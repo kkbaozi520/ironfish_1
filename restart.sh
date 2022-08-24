fff26d2bf17911e5a0821e2bbdf8d78521ca5665
kkbaozi520 <95422814+kkbaozi520@users.noreply.github.com>
2022-08-24 23:21:48 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+count=0
 while [ true ]
 do
+         count_1=$(expr $count % 72)
+         if [ "$count_1" -eq 0 ]
+         then
+               sudo rm -rf /data/ironfish/.ironfish/databases      
+         else
+               echo "keep going"
+         fi 
          sudo docker restart ironfish_node
          sleep 3600
+         count=$(expr $count + 1)
 done
