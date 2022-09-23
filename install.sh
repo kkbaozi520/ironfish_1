27dfc8c40efd392eb433352254d514cfa7bce828
kkbaozi520 <95422814+kkbaozi520@users.noreply.github.com>
2022-09-23 23:36:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ps -ef | grep "restart.sh" | awk '{print $2}' | xargs kill -9
+#ps -ef | grep "restart.sh" | awk '{print $2}' | xargs kill -9
 nohup sh $HOME/ironfish_1/restart.sh >> $HOME/ironfish_1/ironfish.log &
 ls
