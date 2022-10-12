53d20bae6a6ff65d945fbf18957a9aa4cce9b81a
kkbaozi520 <95422814+kkbaozi520@users.noreply.github.com>
2022-10-12 23:26:12 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -4,12 +4,17 @@ while [ true ]
 do
          count_1=$(expr $count % 72)
          if [ "$count_1" -eq 0 ]
-         then
-               sudo rm -rf /data/ironfish/.ironfish/databases      
+         then  
+               sudo mv /data/ironfish/.ironfish/config.json /home/ubuntu
+               sudo rm -rf /data/ironfish
+               sudo docker rm -f ironfish_node
+               sudo docker run -tid --name=ironfish_node --restart always --network host --volume /data/ironfish/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start --upgrade
+               sudo rm -rf /data/ironfish/.ironfish/config.json
+               sudo mv /home/ubuntu/config.json /data/ironfish/.ironfish
+               sudo docker restart ironfish_node      
          else
                echo "keep going"
          fi 
-         sudo docker restart ironfish_node
          sleep 3600
          count=$(expr $count + 1)
 done
